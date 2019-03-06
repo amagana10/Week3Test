efb440d2d9428c702a982d9cfa9e924a20ac36aa
amagana10 <32153064+amagana10@users.noreply.github.com>
2019-03-05 21:06:04 -0500

    Update Room.java

--- app/src/main/java/com/example/week3test/Room.java
+++ app/src/main/java/com/example/week3test/Room.java
@@ -49,48 +49,6 @@ public class Room {
             }
         }
         return false;
-        /* This code works just not really
-        int consecutiveInfectedRooms = 0;
-        for (int i =0;i<floor.length;i++){
-            for (int j =0;j<floor[i].length;j++){
-                if (floor[i][j].isInfected){
-                    consecutiveInfectedRooms = 1;
-                    //check for 5 in a horozontal row
-                    for (int z = 1; z<5 ;z++){
-                        if (j+z<floor[i].length){
-                            if (floor[i][j+z].isInfected){
-                                consecutiveInfectedRooms++;
-                            }
-                        }
-                    }
-                    if (consecutiveInfectedRooms==5){
-                        return true;
-                    }
-                    else {
-                        consecutiveInfectedRooms = 1;
-                    }
-
-                    //check for 5 vertical in a row
-                    for (int z = 1; z<5 ;z++){
-                        if (i+z<floor.length){
-                            if (floor[i+z][j].isInfected){
-                                consecutiveInfectedRooms++;
-                            }
-                        }
-                    }
-                    if (consecutiveInfectedRooms==5){
-                        return true;
-                    }
-                    else {
-                        consecutiveInfectedRooms = 1;
-                    }
-                }
-
-            }
-        }
-
-        return false;*/
-
     }
 
 
